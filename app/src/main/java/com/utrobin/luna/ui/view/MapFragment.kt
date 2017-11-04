ec44d393581e3873e6394aa10c5ebf05a960cbce
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-11-05 01:28:35 +0300

    Integrated YMK into map fragment

--- app/src/main/java/com/utrobin/luna/ui/view/MapFragment.kt
+++ app/src/main/java/com/utrobin/luna/ui/view/MapFragment.kt
@@ -5,8 +5,11 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import butterknife.BindView
 import butterknife.ButterKnife
 import com.utrobin.luna.R
+import com.utrobin.luna.utils.MapControllerWrapper
+import ru.yandex.yandexmapkit.MapView
 
 
 /**
@@ -14,9 +17,22 @@ import com.utrobin.luna.R
  */
 
 class MapFragment : Fragment() {
+    @BindView(R.id.map)
+    lateinit var mapView: MapView
+
+    private lateinit var mapController: MapControllerWrapper
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.map_fragment, container, false)
         ButterKnife.bind(this, view)
         return view
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapController = MapControllerWrapper(mapView)
+        mapController.overlayManager.myLocation.isEnabled = true
+
     }
 }
