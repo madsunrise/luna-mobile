2816f76fc9320985dcb3af9b2d8be7d7808142e8
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-11-12 00:59:07 +0300

    Integrated Crashlytics

--- app/src/main/java/com/utrobin/luna/App.kt
+++ app/src/main/java/com/utrobin/luna/App.kt
@@ -1,6 +1,9 @@
 package com.utrobin.luna
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
+import io.fabric.sdk.android.Fabric
 
 /**
  * Created by ivan on 31.10.2017.
@@ -11,6 +14,14 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         component = DaggerAppComponent.create()
+        configureCrashReporting()
+    }
+
+    private fun configureCrashReporting() {
+        val crashlyticsCore = CrashlyticsCore.Builder()
+                .disabled(BuildConfig.DEBUG)
+                .build()
+        Fabric.with(this, Crashlytics.Builder().core(crashlyticsCore).build())
     }
 
     companion object {
