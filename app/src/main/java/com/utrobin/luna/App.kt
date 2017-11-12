613aaf449a80761e8d189418ae2b2a4799769c2d
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-11-12 15:52:04 +0300

    Integrated Apollo client

--- app/src/main/java/com/utrobin/luna/App.kt
+++ app/src/main/java/com/utrobin/luna/App.kt
@@ -1,9 +1,12 @@
 package com.utrobin.luna
 
 import android.app.Application
+import com.apollographql.apollo.ApolloClient
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
 import io.fabric.sdk.android.Fabric
+import okhttp3.OkHttpClient
+
 
 /**
  * Created by ivan on 31.10.2017.
@@ -14,7 +17,16 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         component = DaggerAppComponent.create()
+        configureApolloClient()
         configureCrashReporting()
+    }
+
+    private fun configureApolloClient() {
+        val okHttpClient = OkHttpClient.Builder().build()
+        apolloClient = ApolloClient.builder()
+                .serverUrl(BASE_URL)
+                .okHttpClient(okHttpClient)
+                .build();
     }
 
     private fun configureCrashReporting() {
@@ -27,5 +39,10 @@ class App : Application() {
     companion object {
         lateinit var component: AppComponent
             private set
+
+        lateinit var apolloClient: ApolloClient
+            private set
+
+        private const val BASE_URL = "https://utrobin.com/api/graphql"
     }
 }
