a7e11a46f24d145cd696770db1038fa02de4f853
Ivan Rudnev <rudnev.vanya@gmail.com>
2018-01-03 17:35:50 +0300

    Added page indicators for ViewPager

--- app/src/main/java/com/utrobin/luna/adapter/FeedAdapter.kt
+++ app/src/main/java/com/utrobin/luna/adapter/FeedAdapter.kt
@@ -1,10 +1,12 @@
 package com.utrobin.luna.adapter
 
+import android.content.Context
 import android.graphics.drawable.PictureDrawable
 import android.net.Uri
 import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewPager
 import android.support.v7.widget.RecyclerView
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,7 +20,6 @@ import com.utrobin.luna.model.FeedItem
 import com.utrobin.luna.utils.svg.GlideApp
 import com.utrobin.luna.utils.svg.SvgSoftwareLayerSetter
 import de.hdodenhof.circleimageview.CircleImageView
-import java.util.*
 
 /**
  * Created by ivan on 31.10.2017.
@@ -65,8 +66,45 @@ class FeedAdapter(items: List<FeedItem>) : FooterLoaderAdapter(ArrayList(items))
 
         // Photos slider
         holder.viewPager.adapter = ViewPagerAdapter(context, item.photos)
+        val totalPages = item.photos.size
+        addBottomDots(context, holder.dotsContainer, 0, totalPages)
+        holder.viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+            override fun onPageSelected(pagePosition: Int) {
+                addBottomDots(context, holder.dotsContainer, pagePosition, totalPages)
+            }
+        })
 
         holder.rating.text = item.stars.toString()
+    }
+
+    private fun addBottomDots(context: Context, container: LinearLayout, currentPage: Int, totalPages: Int) {
+        container.removeAllViews()
+        if (totalPages < 2) {
+            return
+        }
+        val dots = ArrayList<TextView>()
+        for (i in 0 until totalPages) {
+            val textView = TextView(container.context)
+            textView.text =
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                        Html.fromHtml("&#8226;", Html.FROM_HTML_MODE_LEGACY);
+                    } else {
+                        Html.fromHtml("&#8226;")
+                    }
+            textView.textSize = 35f
+
+            val color = if (i != currentPage) {
+                ContextCompat.getColor(context, R.color.view_pager_inactive_dot)
+            } else {
+                ContextCompat.getColor(context, R.color.view_pager_active_dot)
+            }
+            textView.setTextColor(color)
+
+            container.addView(textView)
+            dots.add(textView)
+        }
     }
 
     inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -74,6 +112,7 @@ class FeedAdapter(items: List<FeedItem>) : FooterLoaderAdapter(ArrayList(items))
         val location: TextView = view.findViewById(R.id.location)
         val avatar: CircleImageView = view.findViewById(R.id.avatar)
         val viewPager: ViewPager = view.findViewById(R.id.pager)
+        val dotsContainer: LinearLayout = view.findViewById(R.id.dots_container)
         val signsContainer: LinearLayout = view.findViewById(R.id.signs_container)
         val rating: TextView = view.findViewById(R.id.rating)
     }
