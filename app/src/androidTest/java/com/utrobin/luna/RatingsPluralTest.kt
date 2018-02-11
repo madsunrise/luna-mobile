8664e414a0a1de2b1e5021908e5e7d1a825f8a2a
Ivan Rudnev <rudnev.vanya@gmail.com>
2018-02-11 23:46:38 +0300

    Disabled plural testing on English emulators

--- app/src/androidTest/java/com/utrobin/luna/RatingsPluralTest.kt
+++ app/src/androidTest/java/com/utrobin/luna/RatingsPluralTest.kt
@@ -5,6 +5,7 @@ import android.support.test.runner.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,11 +19,14 @@ class RatingsPluralTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
+        if (Locale.getDefault().language != "ru") {
+            return      // We can't test it on emulator with English locale
+        }
         for (i in 0..500) {
             val testString = appContext.resources.getQuantityString(R.plurals.ratings_count, i, "", i)
-            val lastWord = testString.split("\\s+".toRegex()).last()
-            assertEquals(getWordByQuantity(i).toLowerCase(), lastWord.toLowerCase())
-            println("Quantity: $i, expected value: ${getWordByQuantity(i)}, actual: $lastWord")
+            val actual = testString.split("\\s+".toRegex()).last().toLowerCase()
+            val expected = getWordByQuantity(i).toLowerCase()
+            assertEquals("Testing with quantity = $i,", expected, actual)
         }
     }
 
