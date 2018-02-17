13b32833a33cd08e0a266d4ac458ee8b657dc238
Ivan Rudnev <rudnev.vanya@gmail.com>
2018-02-18 00:08:04 +0300

    Building master layout almost finished

--- app/src/main/java/com/utrobin/luna/ui/view/MasterActivity.kt
+++ app/src/main/java/com/utrobin/luna/ui/view/MasterActivity.kt
@@ -7,7 +7,6 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.MenuItem
-import android.view.View
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -22,7 +21,6 @@ import com.utrobin.luna.model.MasterExtended
 import com.utrobin.luna.network.NetworkError
 import com.utrobin.luna.ui.contract.MasterContract
 import com.utrobin.luna.ui.presenter.MasterPresenter
-import kotlinx.android.synthetic.main.error_container.view.*
 
 
 /**
@@ -51,10 +49,10 @@ class MasterActivity : AppCompatActivity(), MasterContract.View {
         setState(State.LOADING)
 
         presenter.loadData(base)
-        binding.errorContainer!!.repeat_btn.setOnClickListener {
-            setState(State.LOADING)
-            presenter.loadData(base)
-        }
+//        binding.errorContainer!!.repeat_btn.setOnClickListener {
+//            setState(State.LOADING)
+//            presenter.loadData(base)
+//        }
 
         binding.pager.adapter = ViewPagerAdapter(this, base.photos)
 
@@ -81,10 +79,11 @@ class MasterActivity : AppCompatActivity(), MasterContract.View {
     private fun fillViews() {
         binding.title.text = master.base.name
 
-        val reviewsCount = 124
-        binding.rating.text = resources.getQuantityString(R.plurals.ratings_count,
-                reviewsCount, master.base.stars.toString(), reviewsCount)
         drawStars()
+//        binding.ratingMainPart.text = resources.getQuantityString(R.plurals.ratings_count,
+//                reviewsCount, master.base.stars.toString(), reviewsCount)
+        binding.ratingMainPart.text = master.base.stars.toString()
+        binding.ratingSecondaryPart.text = " | 124 оценки"
 
         binding.masterDescription.text = "Мы легко впишемся в ваш график, а все наши услуги" +
                 " не займут у вас много времени."
@@ -92,8 +91,13 @@ class MasterActivity : AppCompatActivity(), MasterContract.View {
         binding.addressMetro.text = "Улица 1905 года"
         binding.addressDescription.text = master.base.address.description
 
+        fillSigns()
         fillReviews()
         fillSuitableMasters()
+
+        binding.buttonWhen.text = "20 янв, 19:15"
+        binding.buttonWhere.text = "Пресненский"
+        binding.buttonWhat.text = "3 услуги"
 
         binding.appointmentPrice.text = "2500 \u20BD"
         binding.appointmentDuration.text = "1ч 20м"
@@ -131,6 +135,61 @@ class MasterActivity : AppCompatActivity(), MasterContract.View {
     }
 
 
+    private fun fillSigns() {
+        val params = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        params.setMargins(0, 0, resources.getDimension(R.dimen.master_space_between_signs).toInt(), 0)
+
+
+        val sign1 = LayoutInflater
+                .from(this)
+                .inflate(R.layout.master_sign, binding.signsContainer, false)
+        sign1.layoutParams = params
+
+        sign1.findViewById<TextView>(R.id.main_part).setCompoundDrawablesWithIntrinsicBounds(
+                ContextCompat.getDrawable(this, R.drawable.ic_cute_rocket_launching),
+                null,
+                null,
+                null)
+        sign1.findViewById<TextView>(R.id.main_part).text = "Быстро"
+        sign1.findViewById<TextView>(R.id.secondary_part).text = " | 15"
+
+        binding.signsContainer.addView(sign1)
+
+
+        val sign2 = LayoutInflater
+                .from(this)
+                .inflate(R.layout.master_sign, binding.signsContainer, false)
+        sign2.layoutParams = params
+
+        sign2.findViewById<TextView>(R.id.main_part).setCompoundDrawablesWithIntrinsicBounds(
+                ContextCompat.getDrawable(this, R.drawable.ic_neatly),
+                null,
+                null,
+                null)
+        sign2.findViewById<TextView>(R.id.main_part).text = "Аккуратно"
+        sign2.findViewById<TextView>(R.id.secondary_part).text = " | 25"
+
+        binding.signsContainer.addView(sign2)
+
+
+        val sign3 = LayoutInflater
+                .from(this)
+                .inflate(R.layout.master_sign, binding.signsContainer, false)
+        sign3.layoutParams = params
+
+        sign3.findViewById<TextView>(R.id.main_part).setCompoundDrawablesWithIntrinsicBounds(
+                ContextCompat.getDrawable(this, R.drawable.ic_pallet),
+                null,
+                null,
+                null)
+        sign3.findViewById<TextView>(R.id.main_part).text = "Большой выбор"
+        sign3.findViewById<TextView>(R.id.secondary_part).text = " | 9"
+
+        binding.signsContainer.addView(sign3)
+
+
+    }
+
     private fun fillReviews() {
         binding.seeAllNReviews.text = "Посмотреть все 34 отзыва"
 
@@ -141,6 +200,28 @@ class MasterActivity : AppCompatActivity(), MasterContract.View {
         params.setMargins(0, 0, resources.getDimension(R.dimen.master_space_between_reviews).toInt(), 0)
 
         val reviewsCount = 4
+
+        val names = ArrayList<String>().apply {
+            add("Псевдопродольная Алефтина")
+            add("Каролина Яцунова")
+            add("Ксения Попова")
+            add("Элла Орбельни")
+        }
+
+        val dates = ArrayList<String>().apply {
+            add("2 дн. назад. Мастер Пенсильвана В.")
+            add("3 дн. назад. Мастер Агафья К.")
+            add("3 дн. назад. Мастер Пенсильвана В.")
+            add("5 дн. назад. Мастер Алия А.")
+        }
+
+        val texts = ArrayList<String>().apply {
+            add("Аккуратный и опрятный мастер, приятный персонал," +
+                    " чистое оборудование и вообще мне все понравилось))")
+            add("Каждый раз когда я посещаю эту клинику первое что бросается мне в лицо это приятные девушки на ресепшне, которые всегда улыбчивы и вежливы. Это настраивает на позитивный лад перед началом любой процедуры. Мне не страшно отдавать себя в руки специалистов Силуэта, поскольку уровень их квалификации не вызывает у меня сомнений.")
+            add("Проблемы с волосами меня преследуют давно, но я никак не могу прекратить их красить ежемесячно, из-за чего постоянно сжигаю. Но в последнее время на ощупь они вообще стали как солома, а на концы и взглянуть было страшно. А в довес ко всему, еще и сожгла кожу головы краской. По совету хорошей подружки решила обратиться к проверенному трихологу в медицинский центр красоты и здоровья. Инна Павловна меня отлично приняла и рассказала, как можно избежать подобных проблем. Более того, за два сеанса она привела кожу в порядок, а с волосами немножко поколдовала и их состояние улучшилось. Так что походом на Весковский я очень довольна.")
+            add("Спасибо большое Виктории за сложную, но на совесть выполненную работу! Особенно за индивидуальный подход, быстроту и после всего перенесенного волосами за их качество. Выровнять темные корни и рыжую полосу по середине волос с белоснежными концами нелегко, но она справились! Очень всем рекомендую этого суперского мастера и чудесного человека!!!! Обязательно приду к Вам еще не раз!")
+        }
 
         for (i in 0 until reviewsCount) {
             val review = LayoutInflater
@@ -153,11 +234,9 @@ class MasterActivity : AppCompatActivity(), MasterContract.View {
 
             binding.reviewsContainer.addView(review)
 
-            review.findViewById<TextView>(R.id.review_title).text = "Псевдопродольная Алефтина"
-            review.findViewById<TextView>(R.id.review_date).text = "2 дн. назад. Мастер Пенсильвана В."
-            review.findViewById<TextView>(R.id.review_text).text = "Аккуратный и опрятный мастер, приятный персонал," +
-                    " чистое оборудование и еще много-много всего, длинный-длинный текст"
-
+            review.findViewById<TextView>(R.id.review_author).text = names[i]
+            review.findViewById<TextView>(R.id.review_date).text = dates[i]
+            review.findViewById<TextView>(R.id.review_text).text = texts[i]
         }
     }
 
@@ -184,7 +263,8 @@ class MasterActivity : AppCompatActivity(), MasterContract.View {
         master1.findViewById<TextView>(R.id.price).text = "2500 \u20BD"
         var ratingsCount = 124
         var rating = 4.7 //item.stars.toString()
-        master1.findViewById<TextView>(R.id.rating).text = resources.getQuantityString(R.plurals.ratings_count, ratingsCount, rating, ratingsCount)
+        master1.findViewById<TextView>(R.id.rating_main_part).text = "4.7"
+        master1.findViewById<TextView>(R.id.rating_secondary_part).text = " | 124 оценки"
         binding.suitableMastersContainer.addView(master1)
 
 
@@ -200,7 +280,8 @@ class MasterActivity : AppCompatActivity(), MasterContract.View {
         master2.findViewById<TextView>(R.id.price).text = "2800 \u20BD"
         ratingsCount = 155
         rating = 4.8 //item.stars.toString()
-        master2.findViewById<TextView>(R.id.rating).text = resources.getQuantityString(R.plurals.ratings_count, ratingsCount, rating, ratingsCount)
+        master2.findViewById<TextView>(R.id.rating_main_part).text = "4.8"
+        master2.findViewById<TextView>(R.id.rating_secondary_part).text = " | 155 оценок"
         binding.suitableMastersContainer.addView(master2)
 
 
@@ -216,9 +297,9 @@ class MasterActivity : AppCompatActivity(), MasterContract.View {
         master3.findViewById<TextView>(R.id.price).text = "3500 \u20BD"
         ratingsCount = 107
         rating = 4.5 //item.stars.toString()
-        master3.findViewById<TextView>(R.id.rating).text = resources.getQuantityString(R.plurals.ratings_count, ratingsCount, rating, ratingsCount)
+        master3.findViewById<TextView>(R.id.rating_main_part).text = "4.5"
+        master3.findViewById<TextView>(R.id.rating_secondary_part).text = " | 107 оценок"
         binding.suitableMastersContainer.addView(master3)
-
     }
 
 
@@ -227,30 +308,31 @@ class MasterActivity : AppCompatActivity(), MasterContract.View {
         presenter.detachView()
     }
 
+    // TODO make loading and error state
     private fun setState(state: State) {
-        when (state) {
-            State.CONTENT -> {
-                with(binding) {
-                    mainContainer.visibility = View.VISIBLE
-                    errorContainer!!.visibility = View.GONE
-                    progressContainer!!.visibility = View.GONE
-                }
-            }
-            State.ERROR -> {
-                with(binding) {
-                    mainContainer.visibility = View.GONE
-                    errorContainer!!.visibility = View.VISIBLE
-                    progressContainer!!.visibility = View.GONE
-                }
-            }
-            State.LOADING -> {
-                with(binding) {
-                    //mainContainer.visibility = View.GONE
-                    errorContainer!!.visibility = View.GONE
-                    //progressContainer!!.visibility = View.VISIBLE
-                }
-            }
-        }
+//        when (state) {
+//            State.CONTENT -> {
+//                with(binding) {
+//                    downloadableContent.visibility = View.VISIBLE
+//                    errorContainer.visibility = View.GONE
+//                    progressBar.visibility = View.GONE
+//                }
+//            }
+//            State.ERROR -> {
+//                with(binding) {
+//                    downloadableContent.visibility = View.GONE
+//                    errorContainer.visibility = View.VISIBLE
+//                    progressBar.visibility = View.GONE
+//                }
+//            }
+//            State.LOADING -> {
+//                with(binding) {
+//                    downloadableContent.visibility = View.GONE
+//                    errorContainer.visibility = View.GONE
+//                    progressBar.visibility = View.VISIBLE
+//                }
+//            }
+//        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
