bf4893528709fb7982a593e33b75b7e5b6a2b860
Ivan Rudnev <rudnev.vanya@gmail.com>
2018-07-27 00:15:57 +0300

    Fragment for User

--- app/src/main/java/com/utrobin/luna/model/User.kt
+++ app/src/main/java/com/utrobin/luna/model/User.kt
@@ -14,19 +14,43 @@ data class User(
         val ctime: Date
 ) : Parcelable {
 
+    constructor(user: MasterQuery.User) : this(
+            id = user.fragments().userWithoutPassword().id().toLong(),
+            email = user.fragments().userWithoutPassword().email(),
+            password = null,
+            role = user.fragments().userWithoutPassword().role(),
+            ctime = Date(user.fragments().userWithoutPassword().ctime().toLong())
+    )
 
-    constructor(user: MasterQuery.User) : this(user.id().toLong(),
-            user.email(), null, user.role(), Date(user.ctime().toLong()))
+    constructor(user: MasterQuery.User1) : this(
+            id = user.fragments().userWithoutPassword().id().toLong(),
+            email = user.fragments().userWithoutPassword().email(),
+            password = null,
+            role = user.fragments().userWithoutPassword().role(),
+            ctime = Date(user.fragments().userWithoutPassword().ctime().toLong())
+    )
 
-    constructor(user: MasterQuery.User1) : this(user.id().toLong(),
-            user.email(), null, user.role(), Date(user.ctime().toLong()))
+    constructor(user: MasterQuery.User2) : this(
+            id = user.fragments().userWithoutPassword().id().toLong(),
+            email = user.fragments().userWithoutPassword().email(),
+            password = null,
+            role = user.fragments().userWithoutPassword().role(),
+            ctime = Date(user.fragments().userWithoutPassword().ctime().toLong())
+    )
 
-    constructor(user: MasterQuery.User2) : this(user.id().toLong(),
-            user.email(), null, user.role(), Date(user.ctime().toLong()))
+    constructor(user: MasterQuery.User3) : this(
+            id = user.fragments().userWithoutPassword().id().toLong(),
+            email = user.fragments().userWithoutPassword().email(),
+            password = null,
+            role = user.fragments().userWithoutPassword().role(),
+            ctime = Date(user.fragments().userWithoutPassword().ctime().toLong())
+    )
 
-    constructor(user: MasterQuery.User3) : this(user.id().toLong(),
-            user.email(), null, user.role(), Date(user.ctime().toLong()))
-
-    constructor(user: MasterQuery.User4) : this(user.id().toLong(),
-            user.email(), null, user.role(), Date(user.ctime().toLong()))
+    constructor(user: MasterQuery.User4) : this(
+            id = user.fragments().userWithoutPassword().id().toLong(),
+            email = user.fragments().userWithoutPassword().email(),
+            password = null,
+            role = user.fragments().userWithoutPassword().role(),
+            ctime = Date(user.fragments().userWithoutPassword().ctime().toLong())
+    )
 }
